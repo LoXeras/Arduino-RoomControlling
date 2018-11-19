e035bb1dea28bac28d8b5b572c7645056e5dd7a6
LoXeras Dev <duff.dario@gmail.com>
2018-11-19 17:26:01 +0100

    Arduino Initial commit
    created Arduino Project with the default files.

---
+++ Arduino Code/RoomControlling/RoomControlling/RoomControlling.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RoomControlling.ino
+ Created:	19.11.2018 17:25:05
+ Author:	duffd
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
