47fcf6b4d2ae94be743c21daf9484cc3801914b1
LoXeras Dev <duff.dario@gmail.com>
2018-12-31 13:45:19 +0100

    Code: Added Header

--- Arduino Code/RoomControlling/RoomControlling/RoomControlling.ino
+++ Arduino Code/RoomControlling/RoomControlling/RoomControlling.ino
@@ -1,15 +1,30 @@
 /*
  Name:		RoomControlling.ino
  Created:	19.11.2018 17:25:05
- Author:	duffd
+ Author:	Dario Duff aka LoXeras
 */
 
-// the setup function runs once when you press reset or power the board
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <stdlib.h>
+
+#define HYT_ADDR 0x28     // I2C address of the HYT 939
+
+
+
+const char* wlan_ssid = "Virus_Distribution_Center";				//WLAN SSID
+const char* wlan_password = "-";									//Password
+
+//MYSQL Host 
+const char* host = "loxeras.com";							
+
+
 void setup() {
 
 }
 
-// the loop function runs over and over again until power down or reset
+
+
 void loop() {
   
 }
