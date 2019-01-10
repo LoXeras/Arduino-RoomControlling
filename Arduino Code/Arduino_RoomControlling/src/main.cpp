ce8eaa3c47de27d650a107bd89bef637136ff624
LoXeras Dev <duff.dario@gmail.com>
2019-01-10 14:15:28 +0100

    MOVED ARDUINO CODE TO PLATTFORMIO
    moved the project to another development plattform, plattformIO for atom.io

--- Arduino Code/Arduino_RoomControlling/src/main.cpp
+++ Arduino Code/Arduino_RoomControlling/src/main.cpp
@@ -4,27 +4,27 @@
  Author:	Dario Duff aka LoXeras
 */
 
+
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <stdlib.h>
 
 #define HYT_ADDR 0x28     // I2C address of the HYT 939
 
-
-
 const char* wlan_ssid = "Virus_Distribution_Center";				//WLAN SSID
 const char* wlan_password = "-";									//Password
 
-//MYSQL Host 
-const char* host = "loxeras.com";							
+//MYSQL Host
+const char* host = "loxeras.com";
 
 
 void setup() {
-
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.printf("HELLO\n" );
 }
 
-
-
 void loop() {
-  
+  // put your main code here, to run repeatedly:
 }
